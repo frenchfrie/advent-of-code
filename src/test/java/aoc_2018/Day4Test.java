141e161815f46cc2f7266597d30e3b47a41fdba1
Henri-Mayeul de Benque <henrimayeul.debenque@concordnow.com>
2019-12-04 16:28:41 +0100

    Update pom and correct test for CI

--- src/test/java/aoc_2018/Day4Test.java
+++ src/test/java/aoc_2018/Day4Test.java
@@ -1,5 +1,6 @@
 package aoc_2018;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 import org.slf4j.Logger;
 
@@ -16,40 +17,43 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class Day4Test {
 
     private static final Logger LOGGER = getLogger(Day4Test.class);
-    
-    public static final Stream<String> TEST_DATA = Stream.of(
-            "[1518-11-01 00:00] Guard #10 begins shift",
-            "[1518-11-01 00:05] falls asleep",
-            "[1518-11-01 00:25] wakes up",
-            "[1518-11-01 00:30] falls asleep",
-            "[1518-11-01 00:55] wakes up",
-            "[1518-11-01 23:58] Guard #99 begins shift",
-            "[1518-11-02 00:40] falls asleep",
-            "[1518-11-02 00:50] wakes up",
-            "[1518-11-03 00:05] Guard #10 begins shift",
-            "[1518-11-03 00:24] falls asleep",
-            "[1518-11-03 00:29] wakes up",
-            "[1518-11-04 00:02] Guard #99 begins shift",
-            "[1518-11-04 00:36] falls asleep",
-            "[1518-11-04 00:46] wakes up",
-            "[1518-11-05 00:03] Guard #99 begins shift",
-            "[1518-11-05 00:45] falls asleep",
-            "[1518-11-05 00:55] wakes");
+
+    @NotNull
+    private static Stream<String> getTestData() {
+        return Stream.of(
+                "[1518-11-01 00:00] Guard #10 begins shift",
+                "[1518-11-01 00:05] falls asleep",
+                "[1518-11-01 00:25] wakes up",
+                "[1518-11-01 00:30] falls asleep",
+                "[1518-11-01 00:55] wakes up",
+                "[1518-11-01 23:58] Guard #99 begins shift",
+                "[1518-11-02 00:40] falls asleep",
+                "[1518-11-02 00:50] wakes up",
+                "[1518-11-03 00:05] Guard #10 begins shift",
+                "[1518-11-03 00:24] falls asleep",
+                "[1518-11-03 00:29] wakes up",
+                "[1518-11-04 00:02] Guard #99 begins shift",
+                "[1518-11-04 00:36] falls asleep",
+                "[1518-11-04 00:46] wakes up",
+                "[1518-11-05 00:03] Guard #99 begins shift",
+                "[1518-11-05 00:45] falls asleep",
+                "[1518-11-05 00:55] wakes");
+    }
 
     @Test
     public void lol1() {
-        Day4.Guard mostAsleepGuard = Day4.findMostAsleepGuard(TEST_DATA);
-        System.out.println("Found guard #" + mostAsleepGuard.getId() + " with a sleep duration of: " + mostAsleepGuard.getSleepDuration().toString());
-        System.out.println("stats:\n" + mostAsleepGuard.printScores());
+        Day4.Guard mostAsleepGuard = Day4.findMostAsleepGuard(getTestData());
+        LOGGER.debug("Found guard #" + mostAsleepGuard.getId() + " with a sleep duration of: " + mostAsleepGuard.getSleepDuration().toString());
+        LOGGER.debug("stats:\n" + mostAsleepGuard.printScores());
         assertEquals(10, mostAsleepGuard.getId());
     }
 
     @Test
     public void lol2() {
-        Day4.Guard guard = Day4.getMostFrequentlyAsleepAtSameMinute(TEST_DATA);
+        Day4.Guard guard = Day4.getMostFrequentlyAsleepAtSameMinute(getTestData());
         Map.Entry<Integer, Integer> maxSleepMinute = guard.getMaxSleepMinute();
-        System.out.println("Found guard #" + guard.getId() + " with a sleep minute: " + maxSleepMinute.getKey() + " at score: " + maxSleepMinute.getValue());
-        System.out.println("stats:\n" + guard.printScores());
+        LOGGER.debug("Found guard #" + guard.getId() + " with a sleep minute: " + maxSleepMinute.getKey() + " at score: " + maxSleepMinute.getValue());
+        LOGGER.debug("stats:\n" + guard.printScores());
         assertEquals(99, guard.getId());
     }
 
